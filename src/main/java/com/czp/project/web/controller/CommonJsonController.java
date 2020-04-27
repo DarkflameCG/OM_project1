06fe5e1070ba04c661e9aeac5eefd29cf73a2763
DarkflameCG <578214764@qq.com>
2020-04-27 15:06:10 +0800

    cgx:add upload

---
+++ src/main/java/com/czp/project/web/controller/CommonJsonController.java
@@ -0,0 +1,52 @@
+package com.czp.project.web.controller;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.czp.project.utils.CodeStatus;
+import com.czp.project.utils.Message;
+
+/**
+ * 用来返回需要json数据的ajax接口
+ * @author mastercgx
+ *
+ */
+@RestController
+@RequestMapping("/commonJson")
+public class CommonJsonController {
+	
+	@RequestMapping("/upload")
+	public ResponseEntity<Message> uploadFile(HttpServletRequest req,
+											  @RequestParam MultipartFile file){
+		String basePath = req.getScheme()+"://"+req.getServerName()+":"+req.getServerPort()+req.getContextPath()+"/";
+		String filename = "";
+		if(file!=null){
+			filename = file.getOriginalFilename();
+			System.out.println("获取到的文件:"+file.getOriginalFilename());
+			//找到一个路径存放文件
+			//String realPath = "I:\\briup1606\\workspace04\\runssm\\WebContent\\file";
+			String realPath = 
+					req.getServletContext().getRealPath("/file");
+			System.out.println(realPath);
+			//创建一个文件，并将上传文件资料传入
+			File dest = new File(realPath, filename);
+			try {
+				file.transferTo(dest);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return ResponseEntity.ok(new Message(CodeStatus.ERROR));
+			}
+		}
+		return ResponseEntity.ok(new Message(CodeStatus.SUCCESS,basePath+"file/"+filename));
+
+		
+	}
+}
